90670d3884aff642f90aa4d43eeb2d4de502852b
SkynetDev <andrejohnson0017@gmail.com>
2021-01-18 23:50:44 -0500

    created creation function

---
+++ create_item.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.7.4;
+
+contract Supplychain {
+
+function createOrder() public{
+
+}
+
+// function payforItem() public {
+
+// }
+
+// function deliverItem() public {
+
+// }
+
+}
+
