aed038c5c301849c76ea8fd8f0546ceed0f82011
SkynetDev <andrejohnson0017@gmail.com>
2021-01-19 23:21:30 -0500

    improved creation function added necessary structs

--- create_item.sol
+++ create_item.sol
@@ -1,18 +1,33 @@
 pragma solidity ^0.7.4;
 
-contract Supplychain {
+contract SupplyChain {
 
-function createOrder() public{
+enum ItemStateChange {Created,Paid,Delivered}
+
+struct S_Item {
+    string _identifier;
+    uint _itemPrice;
+    SupplyChain.ItemStateChange _state;
+}
+
+mapping(uint => S_Item) item;
+
+uint itemIndex;
+
+function createOrder(string memory _identifier, uint _itemPrice) public{
+    item[itemIndex]._identifier = _identifier;
+    item[itemIndex]._itemPrice = _itemPrice;
+    item[itemIndex]._state = ItemStateChange.Created;
+    itemIndex++;
+}
+
+function payforItem() public {
 
 }
 
-// function payforItem() public {
+function deliverItem() public {
 
-// }
-
-// function deliverItem() public {
-
-// }
+}
 
 }
 
