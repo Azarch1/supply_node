eff5e25fcc175537d80e68780a2e157f3899737b
SkynetDev <andrejohnson0017@gmail.com>
2021-01-25 09:31:13 -0500

    fixed struct payment bug for created items

--- create_item.sol
+++ create_item.sol
@@ -30,11 +30,11 @@ function createOrder(string memory _identifier, uint _itemPrice) public{
 }
 
 // Ensures that only an item that has been created can be paid for as well as updates the state to paid after the transaction
-function payforItem(uint _itemIndex, uint _payment) public payable {
-    require(item[_itemIndex]._state == ItemStateChange.Created, "We have an issue processing this request, your item might be further in the chain");
-    require(item[_itemIndex]._itemPrice == _payment, "Only full payments are accepted!");
+function payforItem(uint itemIndex) public payable {
+    require(item[itemIndex]._state == ItemStateChange.Created, "We have an issue processing this request, your item might be further in the chain");
+    require(item[itemIndex]._itemPrice == msg.value, "Only full payments are accepted!");
     item[itemIndex]._state = ItemStateChange.Paid;
-    emit SupplyPaymentandDelivery(msg.sender,address(this), _itemIndex);
+    emit SupplyPaymentandDelivery(msg.sender,address(this), itemIndex);
 }
 
 // Changes the state of the item from paid to delivered in the work flow.
