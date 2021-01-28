185e83f0f174c2bbeabff34965503589293ebfca
SkynetDev <andrejohnson0017@gmail.com>
2021-01-28 10:45:30 -0500

    added withdrawal and owner validation functions

--- create_item.sol
+++ create_item.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.7.4;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
 contract Item {
     uint public priceInWei;
     uint public index;
@@ -7,14 +9,12 @@ contract Item {
     
     SupplyChain parentContract;
     
-    // These variables are populated upon the initialization of this item specific smart contract
     constructor(SupplyChain _parentContract, uint _priceInWei, uint _index) {
         priceInWei = _priceInWei;
         index = _index;
         parentContract = _parentContract;
     }
     
-    // This receive function handles accepting and routing the funds back to the main smart contract
     receive() external payable {
         require(pricePaid == 0, "This item has already been paid for");
         require(priceInWei == msg.value, "Only full payments allowed");
@@ -28,7 +28,7 @@ contract Item {
     }
 }
 
-contract SupplyChain {
+contract SupplyChain is Ownable {
 
 enum ItemStateChange {Created,Paid,Delivered}
 
@@ -47,10 +47,10 @@ uint itemIndex;
 
 event SupplyChainStep(address indexed _from, address _to, string _identifier, uint _itemPrice, address itemAddress);
 
-event SupplyPaymentandDelivery(address indexed _from, address _to, uint indexed itemIndex);
+event SupplyPaymentandDelivery(address indexed _from, address _to, uint indexed itemIndex, address itemAddress);
 
 // Creates the the item leveraging the S_Item struct defined earlier
-function createOrder(string memory _identifier, uint _itemPrice) public{
+function createOrder(string memory _identifier, uint _itemPrice) public onlyOwner{
     Item item = new Item(this, _itemPrice, itemIndex);
     items[itemIndex]._item = item;
     items[itemIndex]._identifier = _identifier;
@@ -65,15 +65,19 @@ function payforItem(uint itemIndex) public payable {
     require(items[itemIndex]._state == ItemStateChange.Created, "We have an issue processing this request, your item might be further in the chain");
     require(items[itemIndex]._itemPrice == msg.value, "Only full payments are accepted!");
     items[itemIndex]._state = ItemStateChange.Paid;
-    emit SupplyPaymentandDelivery(msg.sender,address(this), itemIndex);
+    emit SupplyPaymentandDelivery(msg.sender,address(this), itemIndex, address(items[itemIndex]._item));
 }
 
 // Changes the state of the item from paid to delivered in the work flow.
-function deliverItem(uint itemIndex) public {
+function deliverItem(uint itemIndex) public onlyOwner{
     require(items[itemIndex]._state == ItemStateChange.Paid, "We have an issue processing this request, please ensure your item has been paid for");
     items[itemIndex]._state = ItemStateChange.Delivered;
-    emit SupplyPaymentandDelivery(msg.sender,address(this), itemIndex);
+    emit SupplyPaymentandDelivery(msg.sender,address(this), itemIndex, address(items[itemIndex]._item));
 }
 
+function withdrawFunds(address payable _to, uint _amount) public onlyOwner{
+    require(_amount <= address(this).balance, "There are not enough funds available");
+    _to.transfer(_amount);
+}
 }
 
